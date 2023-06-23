3031ef494fb27b878e9cd306d3eb38da5cea16e3
Anna <annaocirova5@gmail.com>
2023-06-23 09:14:26 +0800

    Именование переменных

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Lab1
 {
@@ -12,13 +13,13 @@ namespace Lab1
 
         static void Main(string[] args)
         {
-            List<TodoItemDTO> lst2 = new List<TodoItemDTO>();//лист, который нужно заполнить обьектами из другого листа 
-            List<TodoItem> lst = new List<TodoItem>(); //другой лист
-            lst.Add(new TodoItem() { Id = 1, Name = "test_1", IsComplete = false, Secret = "secret_1" });
-            lst.Add(new TodoItem() { Id = 2, Name = "test_2", IsComplete = false, Secret = "secret_2" });
-            lst.Add(new TodoItem() { Id = 3, Name = "test_3", IsComplete = false, Secret = "secret_3" });
-            lst.Add(new TodoItem() { Id = 4, Name = "test_4", IsComplete = false, Secret = "secret_4" });
-            foreach (TodoItem e in lst)
+            List<TodoItemDTO> itemDTOs = new List<TodoItemDTO>();//лист, который нужно заполнить обьектами из другого листа 
+            List<TodoItem> items = new List<TodoItem>(); //другой лист
+            items.Add(new TodoItem() { Id = 1, Name = "test_1", IsComplete = false, Secret = "secret_1" });
+            items.Add(new TodoItem() { Id = 2, Name = "test_2", IsComplete = false, Secret = "secret_2" });
+            items.Add(new TodoItem() { Id = 3, Name = "test_3", IsComplete = false, Secret = "secret_3" });
+            items.Add(new TodoItem() { Id = 4, Name = "test_4", IsComplete = false, Secret = "secret_4" });
+           /* foreach (TodoItem e in lst)
             {
                 Console.WriteLine($"Id: {e.Id} - Name: {e.Name} - IsComplete: {e.IsComplete} - Secret: {e.Secret}");
             }
@@ -28,15 +29,16 @@ namespace Lab1
             {
                 TodoItemDTO example = new TodoItemDTO(t);
                 lst2.Add(example);
-            }
-            for (int i = 0; i < lst.Count; i++)
+            }*/
+            for (int i = 0; i < items.Count; i++)
             {
-                TodoItemDTO example = new TodoItemDTO(lst[i]);
-                lst2.Add(example);
+                TodoItemDTO fillingItemDTOs = new TodoItemDTO(items[i]);
+                itemDTOs.Add(fillingItemDTOs);
             }
-            foreach (TodoItemDTO e in lst2)
+            foreach (TodoItemDTO e in itemDTOs)
             {
-                Console.WriteLine($"Id: {e.Id} - Name: {e.Name} - IsComplete: {e.IsComplete} ");
+               // Console.WriteLine($"Id: {e.Id} - Name: {e.Name} - IsComplete: {e.IsComplete} ");
+                Console.WriteLine($"{JsonSerializer.Serialize(e).ToString()}");
             }
         }
     }
